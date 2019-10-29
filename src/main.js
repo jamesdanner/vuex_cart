8b1ce2a405bb29bb915fe706b96b06a8b30a7514
james <1071714548@qq.com>
2019-10-29 15:23:16 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+import Vant from 'vant';
+import 'vant/lib/index.css';
+
+Vue.use(Vant);
 
 new Vue({
   render: h => h(App),
